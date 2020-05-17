4377caef1c53672ceb1ebf251e2e88766908c545
bechco@gmail.com <bechco@gmail.com>
2020-05-17 19:52:52 +0900

    JDBC

--- create-docker-images.sh
+++ create-docker-images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for service in account-service catalog-service frontend-service order-service ; do
-	docker build -t jpetstore-$service $service
+	docker build -t 10.81.208.53:5000/jpetstore-$service $service
+	docker push 10.81.208.53:5000/jpetstore-$service
+	curl -X GET http://localhost:5000/v2/hello-world/tags/list
 done
 
 # end
