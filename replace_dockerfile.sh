17f80d147597f4c61dab29ba81db8eaef516cf40
bechco@gmail.com <bechco@gmail.com>
2020-05-29 17:48:00 +0900

    docker

--- replace_dockerfile.sh
+++ replace_dockerfile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp -f ./docker-compose$1.yaml ./docker-compose.yaml
+ 
 for service in account-service catalog-service frontend-service order-service ; do
 	cp -f ./${service}/Dockerfile_$1 ./${service}/Dockerfile
 done
