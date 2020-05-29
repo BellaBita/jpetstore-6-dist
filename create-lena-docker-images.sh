30070e294ec09497edabbd2352b062b78d2750c0
bechco@gmail.com <bechco@gmail.com>
2020-05-29 17:18:59 +0900

    LENA Image build

--- create-lena-docker-images.sh
+++ create-lena-docker-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for service in account-service catalog-service frontend-service order-service ; do
-	docker build -t 10.81.208.53:5000/jpetstore-$service $service
+	docker build -t 10.81.208.53:5000/jpetstore-$service $service/Dockerfile_lena
 	docker push 10.81.208.53:5000/jpetstore-$service
 	curl -X GET http://localhost:5000/v2/jpetstore-$service/tags/list
 done
