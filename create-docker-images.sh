f6dbc6e0253a40042b97aabb7598b3ff9f85d056
bechco@gmail.com <bechco@gmail.com>
2020-06-01 16:03:59 +0900

    Docker

--- create-docker-images.sh
+++ create-docker-images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 for service in account-service catalog-service frontend-service order-service ; do
-	docker build -t lenasupport/jpetstore-$service $service
-	docker push lenasupport/jpetstore-$service
+	sudo docker build -t lenasupport/jpetstore-$service $service
+	sudo docker push lenasupport/jpetstore-$service
 	#curl -X GET https://registry-1.docker.io/v2/lenasupport/jpetstore-$service/tags/list
 done
 
