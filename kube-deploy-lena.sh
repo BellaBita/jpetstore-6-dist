7ecaa212e0b7dbf260fcbb2b79ceda444a646b1a
bechco@gmail.com <bechco@gmail.com>
2020-06-01 18:23:01 +0900

    kube

--- kube-deploy-lena.sh
+++ kube-deploy-lena.sh
@@ -4,8 +4,15 @@ for service in account catalog frontend order ; do
 	cp  -f ./kube-depoly-service.yaml.template ./${service}-service/
 	cat ./${service}-service/kube-depoly-service.yaml.template | sed "s/%service%/$service/g" > ./${service}-service/kube-depoly-service.yaml
 	cat ./${service}-service/kube-depoly-service.yaml
+	
+	echo "sudo kubectl apply -f ./${service}-service/kube-depoly-service.yaml"
 	sudo kubectl apply -f ./${service}-service/kube-depoly-service.yaml
+	
+	echo "sudo kubectl rollout restart deployment/${service}-service"
 	sudo kubectl rollout restart deployment/${service}-service
+	
+	sudo kubectl get deployment ${service}-service
+	
 done
 
 # end
