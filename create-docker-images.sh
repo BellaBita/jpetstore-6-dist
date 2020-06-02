86fa0e1cda6610c09fac0e798c24e8ac02a0dee3
bechco@gmail.com <bechco@gmail.com>
2020-06-02 13:53:28 +0900

    shell

--- create-docker-images.sh
+++ create-docker-images.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-for service in account-service catalog-service frontend-service order-service ; do
-	sudo docker build -t lenasupport/jpetstore-$service $service
-	sudo docker push lenasupport/jpetstore-$service
-	#curl -X GET https://registry-1.docker.io/v2/lenasupport/jpetstore-$service/tags/list
+if [ -z "${SERVICE_NAMES}" ]; then
+	SERVICE_NAMES="account,catalog,frontend,order"
+fi
+
+IFS=',' read -ra SERVICES <<< "${SERVICE_NAMES}"
+
+for service in "${SERVICES[@]}" ; do
+	serviceName=${service}-service
+	sudo docker build -t 10.81.208.53:5000/jpetstore-$serviceName $serviceName
+	sudo docker push 10.81.208.53:5000/jpetstore-$serviceName
+	#curl -X GET http://10.81.208.53:5000/v2/jpetstore-$serviceName/tags/list
+	#curl -X GET https://registry-1.docker.io/v2/lenasupport/jpetstore-$serviceName/tags/list
 done
 
 # end
