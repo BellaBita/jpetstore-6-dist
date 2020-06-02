699565ad4ee9ee67bc865e3d71ac468e531e4e18
bechco@gmail.com <bechco@gmail.com>
2020-06-02 13:58:08 +0900

    shell

--- kube-deploy-lena.sh
+++ kube-deploy-lena.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-for service in account catalog frontend order ; do
+f [ -z "${SERVICE_NAMES}" ]; then
+	SERVICE_NAMES="account,catalog,frontend,order"
+fi
+
+IFS=',' read -ra SERVICES <<< "${SERVICE_NAMES}"
+
+for service in "${SERVICES[@]}" ; do
 	cp  -f ./kube-depoly-service.yaml.template ./${service}-service/
 	cat ./${service}-service/kube-depoly-service.yaml.template | sed "s/%service%/$service/g" > ./${service}-service/kube-depoly-service.yaml
 	cat ./${service}-service/kube-depoly-service.yaml
 	
+	echo ""
 	echo "sudo kubectl delete -f ./${service}-service/kube-depoly-service.yaml"
 	sudo kubectl delete -f ./${service}-service/kube-depoly-service.yaml
 	
+	echo ""
 	echo "sudo kubectl apply -f ./${service}-service/kube-depoly-service.yaml"
 	sudo kubectl apply -f ./${service}-service/kube-depoly-service.yaml
 	
